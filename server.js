7a01f5ac1eeebffde8d817e23ea8504cb023bf34
Lauren Wollaston <laurenwollaston@outlook.com>
2023-05-25 13:57:46 -0500

    forgor process.env.PORT �

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const app = express();
 const path = require('path');
 const routes = require('./controllers')
-const port = 3001; 
+const port = process.env.PORT || 3001; 
 
 // Middleware
 app.use(express.json()); // Parse JSON requests
